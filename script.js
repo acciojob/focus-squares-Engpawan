d44ef94d1ea6db2ad79ae9bd6373db8781389f1c
Pawan_Sharma <123291452+Engpawan@users.noreply.github.com>
2025-07-05 21:05:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let sqaure1 = document.getElementById("sqaure1")
+let sqaure2 = document.getElementById("sqaure2")
+let sqaure3 = document.getElementById("sqaure3")
+
+
+function mouseenter()
+{
+	sqaure2.style.backgroundColor = "#6F4E37"
+	sqaure2.style.backgroundColor = "#6F4E37"
+	
+}
+
+function mouseenter2()
+{
+	sqaure1.style.backgroundColor = "#6F4E37"
+	sqaure3.style.backgroundColor = "#6F4E37"
+	
+}
+
+function mouseenter3()
+{
+	sqaure1.style.backgroundColor = "#6F4E37"
+	sqaure2.style.backgroundColor = "#6F4E37"
+	
+}
+
+function mouseleave()
+{
+	sqaure1.style.backgroundColor = "#E6E6FA"
+	sqaure2.style.backgroundColor = "#E6E6FA"
+	sqaure3.style.backgroundColor = "#E6E6FA"
+	
+}
