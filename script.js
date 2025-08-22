6777df11e961020d941d537c0e4c3da609ec70df
Pawan_Sharma <123291452+Engpawan@users.noreply.github.com>
2025-08-22 16:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 //your JS code here. If required.
-let sqaure1 = document.getElementById("sqaure1")
-let sqaure2 = document.getElementById("sqaure2")
-let sqaure3 = document.getElementById("sqaure3")
+let box1 = document.getElementById("square1")
+let box2 = document.getElementById("square2")
+let box3 = document.getElementById("square3")
+let box = document.getElementsByClassName("square")
+
+box1.addEventListener("mouseenter",() => {
+	box2.style.backgroundColor = "#6F4E37"
+	box3.style.backgroundColor = "#6F4E37"
+})
 
 
-function mouseenter()
-{
-	sqaure2.style.backgroundColor = "#6F4E37"
-	sqaure3.style.backgroundColor = "#6F4E37"
-	
-}
+box2.addEventListener("mouseenter",() => {
+	box1.style.backgroundColor = "#6F4E37"
+	box3.style.backgroundColor = "#6F4E37"
+})
 
-function mouseenter2()
-{
-	sqaure1.style.backgroundColor = "#6F4E37"
-	sqaure3.style.backgroundColor = "#6F4E37"
-	
-}
 
-function mouseenter3()
-{
-	sqaure1.style.backgroundColor = "#6F4E37"
-	sqaure2.style.backgroundColor = "#6F4E37"
-	
-}
+box3.addEventListener("mouseenter",() => {
+	box1.style.backgroundColor = "#6F4E37"
+	box2.style.backgroundColor = "#6F4E37"
+})
 
-function mouseleave()
-{
-	sqaure1.style.backgroundColor = "#E6E6FA"
-	sqaure2.style.backgroundColor = "#E6E6FA"
-	sqaure3.style.backgroundColor = "#E6E6FA"
-	
-}
+for(let i = 0; i<box.length; i++)
+	{
+		box[i].addEventListener("mouseleave", () => {
+			for(let j = 0; j<box.length; j++)
+				{
+					box[j].style.backgroundColor = "#E6E6FA"
+				}
+		})
+	}
+
+
+
